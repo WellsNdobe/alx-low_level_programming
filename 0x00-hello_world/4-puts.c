2337aa3a1b8c7f9a0ffe2f84ea24e6cc772b750b
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-13 12:09:39 -0700

    fifth task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a line with a puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
