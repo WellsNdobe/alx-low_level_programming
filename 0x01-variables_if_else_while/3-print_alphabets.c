f64a08fbd42c47afe3e1ab31eaeb366b4877e35d
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-14 20:42:46 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * main - entry point to a program that prints alphabets
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char a;
+
+	c = 'a';
+	a = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("\n");
+	return (0);
+}
