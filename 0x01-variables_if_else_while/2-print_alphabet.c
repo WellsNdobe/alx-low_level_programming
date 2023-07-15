168fb1d717fadf9c0626a3bfdf7e344537956b70
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-14 20:31:44 -0700

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - entry point
+ * Return:0 (success)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar("\n");
+	return (0);
+}
