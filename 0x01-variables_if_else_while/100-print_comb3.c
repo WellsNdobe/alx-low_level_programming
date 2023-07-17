1841cc8d579db5b85c646a05638e1ef44b940370
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-17 13:14:36 -0700

    task 2 advanced complete

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,29 +9,36 @@ int main(void)
 {
 	int num1;
 	int num2;
+	int num3;
 
 	num1 = 0;
 	num2 = 1;
+	num3 = 2;
 
 	while (num1 < 9)
 	{
 		num2 = num1 + 1;
 		while (num2 < 10)
 		{
-		putchar(num1 + '0');
-		putchar(num2 + '0');
-		if (num1 == 8 && num2 == 9)
-		{
-			num2++;
-		}
-		else
-		{
-		putchar(',');
-		putchar(' ');
-		}
+			num3 = num2 + 1;
+			while (num3 < 10)
+			{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num2 == 8 && num3 == 9)
+			{
+			num3++;
+			}
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			num3++;
+			}
 		num2++;
 		}
-		num1++;
+	num1++;
 	}
 	putchar('\n');
 	return (0);
