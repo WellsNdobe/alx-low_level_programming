49d9383d5feba3154e90053a92fd38cfdd6787fb
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-17 12:13:07 -0700

    fixed all tasks hopefully

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,21 +7,13 @@
 
 int main(void)
 {
-	char c;
 	int i;
 
 	i = 0;
-	c = 'a';
 	while (i < 10)
 	{
 		putchar(i + '0');
 		i++;
-	}
-	while (c <= 'f')
-	{
-		putchar(c);
-		c++;
-
 	}
 	putchar('\n');
 	return (0);
