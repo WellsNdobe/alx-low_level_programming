9011535cd8e5efbf15f870dac5403f3ba420ce05
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-17 13:43:11 -0700

    clever ways

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to a program that prints combinations of numbers.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	num1 = 0;
+	num2 = 1;
+	num3 = 2;
+
+	while (num1 < 9)
+	{
+		num2 = num1 + 1;
+		while (num2 < 10)
+		{
+			num3 = num2 + 1;
+			while (num3 < 10)
+			{
+			putchar(num1 + '0');
+			putchar(num3 + '0');
+			if (num1 == 7 && num2 == 8 && num3 == 9)
+			{
+			num3++;
+			}
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			num3++;
+			}
+		num2++;
+		}
+	num1++;
+	}
+	putchar('\n');
+	return (0);
+}
