444838d535cbee0bd7860f22878d2164595016c3
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-17 10:50:52 -0700

    sixth task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to a program that prints alphabets
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d \n", i);
+		i++;
+	}
+	return (0);
+}
