748ced3dcfee177e93c011778ed25febd13f118f
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-25 11:35:43 -0700

    task 2 complete

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void swap_int - swaps values of integers
+ * @a: first integer to be swapped
+ * @b: second integer to be swapped
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
