5fbc93727e4986afe43869b40c585beb388b8c66
Daniel Ndobe <danielndobe850@gmail.com>
2023-07-25 12:47:15 -0700

    finally done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,9 +3,8 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- *
+ * main - generates random valid
+ * passwords
  * Return: Always 0 (Success)
  */
 int main(void)
