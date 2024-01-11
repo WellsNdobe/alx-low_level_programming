b8ce9aecc39a0eea7d455b539351b361c9afdfb5
Wells <danielndobe850@gmail.com>
2024-01-11 06:08:24 -0800

    added a default return

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,9 +12,14 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if(y > 0)
+	if (y > 0)
 	{
 	 return (x *= _pow_recursion(x, y-1));
 	}
-	return (x);
+	if (y == 0)
+	{
+	return (1);
+	}
+
+	return (0);
 }
