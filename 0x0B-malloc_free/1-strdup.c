b30e885393ffdfe7a19be8b3d6434394ed33dc87
Wells <danielndobe850@gmail.com>
2024-01-15 18:30:38 -0800

    duplicated a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *_strdup - duplicates a string
+ *@str: string to be duplicated
+ *
+ */
+char *_strdup(char *str)
+{
+
+	char *copy;
+	int i, x;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	i++;
+
+	copy = malloc(sizeof(char)*  (i + 1));
+
+	if (copy == NULL)
+	return (NULL);
+
+	x = 0;
+	while (str[x])
+	{
+	copy[x] = str[x];
+	x++;
+	}
+	copy[x] = '\0';
+
+	return (copy)
+}
