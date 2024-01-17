d65d00cff52b09d9b8a6c7197bce7807dc817a08
Wells <danielndobe850@gmail.com>
2024-01-17 00:13:43 -0800

    New task completed

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints number of command line arguments
+ *@argc: number to be printed
+ *@argv: argument unused in the program
+ *Return: returns 0 if succesful, or -1 when failed
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(argv[i][0] >= '0' && argv[i][0] <= '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
