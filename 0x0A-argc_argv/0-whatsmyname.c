b36360e5d26a94e0d79c4b195d076f81185645e7
Wells <danielndobe850@gmail.com>
2024-01-16 22:33:53 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - prints program name
+ *Return: 0 on success or -1 if unsuccesfu
+ *argc - number of command line arguments
+ *argv - command line arguments as strings
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s", argv[0]);
+	return (0);
+}
