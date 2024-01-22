c76b9a7a600591742865196da6c5ca5a2bf1acce
Wells <danielndobe850@gmail.com>
2024-01-22 11:05:27 -0800

    sum macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - calculates absolute value
+ * @x: value to be added
+ * @y: value to be added
+ * Return: returns absolute value of x
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
