6792d0ac0cd55b254f6733a6311ea1a70e51f688
Wells <danielndobe850@gmail.com>
2024-01-22 10:57:55 -0800

    fixes

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,22 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
- *ABS - calculates absolute value
- *@x: value to calculate absolute for
- *Return: returns x
- */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
-int ABS(int x)
-{
-	if (x < 0)
-	{
-	return (-x);
-	}
-	else
-	{
-	return (x);
-	}
-	return (0);
-}
 #endif
