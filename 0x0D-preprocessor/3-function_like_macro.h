def3cf2ccf17d676c9067a66a90ca0fc9188dce1
Wells <danielndobe850@gmail.com>
2024-01-22 10:38:07 -0800

    added ABS function

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,22 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ *ABS:calculates absolute value
+ *@x: value to calculate absolute for
+ *Return: returns x
+ */
+
+int ABS(int x)
+{
+	if (x < 0)
+	{
+	return (-x)
+	}
+	else
+	{
+	return (x);
+	}
+	return (0);
+}
+#endif FUNCTION_LIKE_MACRO_H
