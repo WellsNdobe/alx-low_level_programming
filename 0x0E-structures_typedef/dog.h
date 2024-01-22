a1f7f28e0c6582c05e541acd8d51f7f94a17317d
Wells <danielndobe850@gmail.com>
2024-01-22 13:08:58 -0800

    debugging

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - a structure for a dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: the name of the person who owns the dog
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 
-}
+};
 
 #endif
